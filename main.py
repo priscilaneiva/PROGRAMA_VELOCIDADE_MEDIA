dd06e3d51fc24d11bc2d7e098280fdc2a7d7ddaf
Priscila Neiva <tecnico.priscila.neiva@gmail.com>
2026-05-15 20:56:52 -0300

    Calculos de velocidade média.

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+distancia = float(input("Escreva a distância em metros, colocando ponto no lugar da virgula e tecle ENTER:"))
+tempo = float(input("Escreva o tempo em segundos, colocando ponto no lugar da virgula e tecle ENTER:"))
+
+velocidadeMedia = distancia/tempo
+
+print(f"A velocidade média é de {velocidadeMedia:.2f} m/s.")
